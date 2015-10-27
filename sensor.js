fb9deaca9769a1472524f4bd2b109204590d7ccd
root <root@server01.host.unixoid.su>
2015-10-27 18:00:56 +0000

    Now verbose can be monitored via Socket.IO at following mountpoint: http://<host>:3080/log

--- sensor.js
+++ sensor.js
@@ -1,6 +1,14 @@
+// Database
 var db	= require('./database.js')('netflow.dat').init();
+
+// Network components
 var udp	= require('dgram').createSocket('udp4');
+var sock = require('socket.io')(3080);
+var log = sock.of('/log');
+
+// etc...
 var nFlow = require('./netflow.js');
+var util = require('util');
 
 udp.bind(9996);
 udp.on('listening',function(){
@@ -12,5 +20,7 @@ udp.on('listening',function(){
     })
     .on('message',function(msg,rinfo){
 	console.log('Received %d bytes from %s',msg.length,rinfo.address);
+	log.emit('message',util.format('Received %d bytes from %s',msg.length,rinfo.address));
+
 	db.save(nFlow(msg),rinfo.address);
     });
